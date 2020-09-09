e27bb2d69c6af807ce0f3b0cd42399308b8bc4da
xabder <xabder@gmail.com>
2020-09-09 22:50:31 +0200

    CSS Changes and new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { MessageboardComponent } from './messageboard/messageboard.component';
 import { MatchingComponent } from './matching/matching.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { UserdataComponent } from './userdata/userdata.component';
 
 const routes: Routes = [
   {path: 'messageboard', component: MessageboardComponent},
   {path: 'matching', component: MatchingComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'userdata', component: UserdataComponent},
 ];
 
 @NgModule({
